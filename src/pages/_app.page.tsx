4cf90b2c1330469d21113ac1cfcf5968b8d71f3f
Mariana <marianacastrorc@gmail.com>
2024-11-01 21:29:45 -0300

    fix: Fixing toast styles

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 import { SessionProvider } from 'next-auth/react'
 import { StyledToastContainer } from './home/styles'
+import 'react-toastify/dist/ReactToastify.css'
 
 globalStyles()
 
